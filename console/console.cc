e15f3c9af6273ad031adad91795a142fd2af1dcd
natal <natal@CLOWN>
2023-09-19 09:33:01 +0400

    complete_no_test_and_no_complex

--- console/console.cc
+++ console/console.cc
@@ -7,9 +7,19 @@ using namespace std;
 
 int main() {
 
-	Polynom <int> a(5);
-	//int* c = new int [5] {5, 1, 2, 3, 4};
-	//Polynom <int> b(c, 5);
-	cout << "hello world";
-	return 0;
+	Polynom<int> a(7);
+	Polynom<int> b(7);
+	a.set(2, 0);
+	a.set(6, 1);
+	a.set(6, 4);
+	b.set(2, 1);
+	b.set(6, 6);
+	cout << a << endl;
+	cout << "------------" << endl;
+	a.shrink_to_fit();
+	cout << a << endl;
+	cout << "------------" << endl;
+	a.expand(10);
+	cout << a;
+
 }
--- include/polynom/polynom.h
+++ include/polynom/polynom.h
@@ -8,16 +8,115 @@ namespace pol {
          T* _data;
          size_t _size;
       public:
-          Polynom(size_t size);
-          Polynom(T* data, size_t size);
-          size_t size() const;
-          ~Polynom(); 
-          Polynom(const Polynom& a); 
-          Polynom<T>& operator=(const Polynom<T> a);
-          void swap(Polynom& a);
-          T& operator[](size_t index);
-          /*friend std::ostream& operator <<(std::ostream& stream, const Polynom<T>& a);*/
-          /*void set(T data, int index);*/
+          Polynom(size_t size) : _size(size), _data(new T[size]()) {}
+          Polynom(T* data, size_t size) :  _data(data), _size(size) {}
+          size_t size() const {
+              return _size;
+          }
+          ~Polynom() {
+              delete [] _data;
+              _data = nullptr;
+              _size = 0;
+          }
+          Polynom(const Polynom& a) : _data(new T[a._size]), _size(a._size) {
+              for (size_t i = 0; i < _size; i++) {
+                  _data[i] = a[i];
+              }
+          }
+          Polynom<T>& operator=(Polynom<T> a) {
+              swap(a);
+              return *this;
+          }
+          void swap(Polynom<T>& a) {
+              std::swap(_data, a._data);
+              std::swap(_size, a._size);
+          }
+          T operator[](size_t index) const {
+              if (_size <= index) {
+                  return 0;
+              }
+              return _data[index];
+          }
+          void set(T data, size_t index) {
+              _data[index] = data;
+          }
+          void expand(size_t size) {
+              auto temp = (new T[size]());
+              for (size_t i = 0; i < _size; i++) {
+                  temp[i] = _data[i];
+              }
+              delete[] _data;
+              _data = temp;
+              _size = size;
+          }
+          Polynom<T>& operator+= (const Polynom<T>& a) {
+              if (a.size() > _size) {
+                  expand(a.size());
+              }
+              for (size_t i = 0; i < a._size; i++) {
+                  _data[i] += a[i];
+              }
+              return *this;
+          }
+          Polynom<T>& operator-= (const Polynom<T>& a) {
+              if (a.size() > _size) {
+                  expand(a.size());
+              }
+              for (size_t i = 0; i < a._size; i++) {
+                  _data[i] -= a[i];
+              }
+              return *this;
+          }
+          Polynom<T> operator- (const Polynom<T>& a) const {
+              Polynom<T> copy(*this);
+              return copy -= a;
+          }
+          Polynom<T> operator+ (Polynom<T> a)  const {
+              return a+=*this;
+          }
+          Polynom<T>& operator*(const T& a) {
+              for (size_t i = 0; i < _size; i++) {
+                  _data[i] *= a;
+              }
+              return *this;
+          }
+          T calculation_polynom_x(const T& x) {
+              T sum = 0;
+              for (size_t i = 0; i < _size; i++) {
+                  sum += _data[i] * pow(x, i);
+              }
+              return sum;
+          }
+          void shrink_to_fit(){
+              for (size_t i = _size-1; i > 0; i--) {
+                  if (_data[i] != 0) {
+                      i++;
+                      auto temp = (new T[i]());
+                      for (size_t j = 0; j < i; j++) {
+                          temp[j] = _data[j];
+                      }
+                      delete[] _data;
+                      _data = temp;
+                      _size = i;
+                      break;
+                  }
+              }
+          }
     };
 
+    template<typename T>
+    std::ostream& operator << (std::ostream& stream, const Polynom<T>& a) {
+	    for (size_t i = 0; i < a.size(); ++i) {
+	    	if (i == 0) {
+		    	stream << a[i];
+		    }
+		    else if (i == 1) {
+			    stream << " + " << a[i] << "*x";
+		    }
+		    else {
+		    	stream << " + " << a[i] << "*x^" << i;
+	    	}
+	    }
+	    return stream;
+    }
  }
